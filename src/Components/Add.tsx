993132b7384b1471717880888d1d5ec6611554de
KANISHK KUMAR <51372334+kanishkkumar666@users.noreply.github.com>
2022-04-16 16:45:04 +0530

    Update Add.tsx

--- src/Components/Add.tsx
+++ src/Components/Add.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
+//import { useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 import { useCreateUserMutation } from "../features/counter/userSlicer";
